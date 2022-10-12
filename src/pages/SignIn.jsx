30c8d4f5908599f784b4e09c14de7991358089d8
egresa <egresameto17@gmail.com>
2022-10-12 10:43:45 +0200

    complete sign-in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,24 +1,40 @@
-import React ,{useState}from 'react'
-import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-import OAuth from '../components/OAuth';
+import { useState } from 'react'
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
+import { Link, useNavigate } from 'react-router-dom'
+import OAuth from '../components/OAuth'
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { toast } from 'react-toastify'
 
 export default function SignIn() {
-  const [showPassword, setShowPassword]=useState(false);
+  const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
-    emial:"",
-    password:""
-  });
-  const {email,password} = formData;
-function onChange(e){
-setFormData((prevState)=>({
-  ...prevState,
-  [e.target.id]:e.target.value,
-}))
-}
-
-
-  
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
+  const navigate = useNavigate()
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+  async function onSubmit(e) {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      if (userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error('Bad user credentials')
+    }
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
@@ -31,15 +47,14 @@ setFormData((prevState)=>({
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type='email'
               id='email'
               value={email}
               onChange={onChange}
               placeholder='Email address'
-              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
-              rounded transition ease-in-out'
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
             />
             <div className='relative mb-6'>
               <input
@@ -48,8 +63,7 @@ setFormData((prevState)=>({
                 value={password}
                 onChange={onChange}
                 placeholder='Password'
-                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
-              rounded transition ease-in-out'
+                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
               />
               {showPassword ? (
                 <AiFillEyeInvisible
@@ -65,10 +79,10 @@ setFormData((prevState)=>({
             </div>
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
               <p className='mb-6'>
-                Don't have an account?
+                Don't have a account?
                 <Link
                   to='/sign-up'
-                  className='text-red-600 hover:text-rose-700 transition duration-200 ease-in-out ml-1'
+                  className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1'
                 >
                   Register
                 </Link>
@@ -76,21 +90,20 @@ setFormData((prevState)=>({
               <p>
                 <Link
                   to='/forgot-password'
-                  className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out '
+                  className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'
                 >
-                  Forgot Password?
+                  Forgot password?
                 </Link>
               </p>
             </div>
-
             <button
-              className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-800 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'
+              className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'
               type='submit'
             >
               Sign in
             </button>
-            <div className=' flex items-center my-4 before:border-t  before:flex-1  before:border-gray-300 after:border-t  after:flex-1 after:border-gray-300'>
-              <p className='text-center font-semibold mx-4  '>OR</p>
+            <div className='flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
+              <p className='text-center font-semibold mx-4'>OR</p>
             </div>
             <OAuth />
           </form>
